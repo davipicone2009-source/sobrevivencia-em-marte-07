825f3c9007292c49de91b8f9fe9e127cb29b576a
I1HN <davipicone2009@gmail.com>
2026-03-18 09:54:10 -0300

    Tentando forçar conflito

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let oxigenio =Number(prompt("insira a quantidade de oxigenio restante (em litros):"));
+alert("oii");
 let pessoas = Number (prompt("insira a quantidade de pessoas em marte:"));
 const cosumoDiario = 15; // litros por pessoa.
 
